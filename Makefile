147b81fe7da0e81ea13fbe9c50800db0d214d084
Mickael <mteaudors@outlook.com>
2019-12-29 13:32:28 +0100

    full attack

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -o cpa.out cpa.c -g -lm -O3 -fopenmp
--- cpa.c
+++ cpa.c
@@ -13,68 +13,126 @@ int read_data(char *path, void *buffer, int size_element, int nb_element) {
 
 int main(int argc, char *argv[]) {
 
+    // To measure the time
+    clock_t start, end;
+    double cpu_time_used;
+
+    // define the number of threads to use 
+    omp_set_num_threads(NB_THREAD);
+
     // byte of the key to attack
-    u_int8_t bnum = 1;
-    // *** TODO *** Add a loop for bnum = 1 to 16
+    u_int8_t bnum;
     
     // Plaintext
     u_int8_t PT[16*NB_TRACES]; 
 
-    // Key
-    u_int8_t key[16];   
+    // Correct key
+    u_int8_t key[16];  
 
+    // Guess key
+    u_int8_t guess[16]; 
+
+    // Raw values of power consuption
     double *traces = (double*)malloc(sizeof(double)*N);    
 
-    // matrix for model values
-    u_int8_t *P = (u_int8_t*)malloc(sizeof(u_int8_t*)*NB_TRACES*256);         
-
-    // matrix for Pearson coefficients         
-    double *R = (double*)malloc(sizeof(double)*NB_SAMPLES*256);                  
-
-    // vector for the mean of the HW according to each key value (mean on each colum of P)
-    double H_mean[256];                          
+    // vector for the mean of the power consuption at each instant
+    double T_mean[NB_SAMPLES];                        
 
     // read data from files
     read_data("traces.raw",traces,sizeof(double),N);
     read_data("plain.raw",PT,16*sizeof(u_int8_t),NB_TRACES);
     read_data("key.raw",key,16*sizeof(u_int8_t),1);
 
-    // Generate data using the model ( construct P )
-    for(int i = 0 ; i < NB_TRACES ; ++i) {
-        for(int k = 0 ; k < 256 ; ++k) {
-            P[i*256 + k] = HW[intermediate(PT[i*16 + bnum - 1],k)];
-        }
-    }
-
-    // for(int i = 0 ; i < 2 ; ++i) {
-    //     for(int k = 0 ; k < 256 ; ++k) {
-    //         printf("%d ",P[i*256+k]);
-    //     }
-    //     printf("\n");
-    // }
-
-    // Compute iteratively the mean of the HW for each key value
-    for(int k = 0 ; k < 256 ; ++k) {
-        H_mean[k] = 0;
-        for(int i = 0 ; i < NB_TRACES ; ++i) {
-            H_mean[k] = H_mean[k] + (P[i*256+k]-H_mean[k])/(i+1); 
+    // Compute iteratively the mean of the power consuption at each instant
+    for(int i = 0 ; i < NB_SAMPLES ; ++i) {
+        T_mean[i] = 0;
+        for(int l = 0 ; l < NB_TRACES ; ++l) {
+            T_mean[i] = T_mean[i] + (traces[l*NB_SAMPLES+i]-T_mean[i])/(l+1); 
         } 
-    }
+    }   
 
-    for(int i = 0 ; i < 256 ; ++i) {
-        printf("%f ",H_mean[i]);
-    }
+    // start timer
+    start = clock();
 
-    printf("\n");
-    printf("Correct key : ");
+    // loop over the 16 bytes of the key
+    #pragma omp parallel for
+    for(bnum = 1 ; bnum<=16; ++bnum) {
+
+        // matrix for model values
+        u_int8_t *P = (u_int8_t*)malloc(sizeof(u_int8_t*)*NB_TRACES*256);         
+
+        // vector for the mean of the HW according to each key value (mean on each colum of P)
+        double H_mean[256];  
+        
+        // Generate data using the model ( construct P )
+        for(int i = 0 ; i < NB_TRACES ; ++i) {
+            for(int k = 0 ; k < 256 ; ++k) {
+                P[i*256 + k] = HW[intermediate(PT[i*16 + bnum - 1],k)];
+            }
+        }
+
+        // Compute iteratively the mean of the HW for each key value
+        for(int k = 0 ; k < 256 ; ++k) {
+            H_mean[k] = 0;
+            for(int i = 0 ; i < NB_TRACES ; ++i) {
+                H_mean[k] = H_mean[k] + (P[i*256+k]-H_mean[k])/(i+1); 
+            } 
+        }
+
+        // Compute Pearson correlation coefficients ( matrix R )
+
+        double max = 0;
+        double A,B,C;
+    
+        for(int i = 0 ; i < NB_SAMPLES ; ++i) {
+            for(int k = 0 ; k < 256 ; ++k) {
+
+                A = 0;
+                B = 0; 
+                C = 0;
+                
+                for(int l = 0 ; l < NB_TRACES ; ++l) {
+                    A += (P[l*256+k] - H_mean[k])*(traces[l*NB_SAMPLES+i] - T_mean[i]);
+                    B += (P[l*256+k] - H_mean[k])*(P[l*256+k] - H_mean[k]);
+                    C += (traces[l*NB_SAMPLES+i] - T_mean[i])*(traces[l*NB_SAMPLES+i] - T_mean[i]);
+                }
+                double r = fabs(A/(sqrt(B*C)));
+                if(r > max) {
+                    max = r;
+                    guess[bnum-1] = k;
+                }
+            }
+        }
+        free(P);
+    }
+    
+    // end timer
+    end = clock();
+
+    printf("Correct key : \t");
     for(int i = 0 ; i < 16 ; ++i) {
         printf("%02x ",key[i]);
     }
     printf("\n");
     
-    free(traces);
-    free(P);
-    free(R);
+    printf("Guess : \t");
+    for(int i = 0 ; i < 16 ; ++i) {
+        printf("%02x ",guess[i]);
+    }
+    printf("\n");
 
+    double success;
+    for(int i = 0 ; i < 16 ; ++i) {
+        success += (key[i] == guess[i]);
+    }
+
+    success = (success/16)*100;
+    printf("Success attack : %0.1f %%\n",success);
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Total Computation time : %0.2f s\n",cpu_time_used);
+    printf("Average time per thread : %0.2f s\n",cpu_time_used/NB_THREAD);
+    free(traces);
+    
     return 0;
 }
